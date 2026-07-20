af4fbd2b6594859c2bc6d1f38156682a7ebc11c9
satogris100-del <satogris100@gmail.com>
2026-07-20 12:11:46 +0700

    project

--- config/database.php
+++ config/database.php
@@ -5,5 +5,19 @@
     $password = '';
 
     
+    try{
+        $db = new PDO(
+            "mysql:host=$host;dbname=$db;charset=utf8mb4",
+            $username, $password,
+            [ 
+                PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+            
+        );
+    }catch (PDOException $e){
+        die(json_encode(['status'=>false, 'message'=>'Error connection Database!']));
+    }
+    
 
 ?>
